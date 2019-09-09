2707fb79ab7a84e641db03d835142185fafb3340
RGasion <53403285+RGasion@users.noreply.github.com>
2019-09-09 14:56:21 +0900

    실습2 제공자료 업로드

---
+++ Assignment1/Assignment1/Assignment1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Assignment1
+{
+    public static class Assignment1
+    {
+        public static int PrintIntegers(StreamReader READER, StreamWriter WRITER)
+        {
+            int integers = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(integers);
+            return 0;
+        }
+
+        public static int PrintStats(StreamReader READER, StreamWriter WRITER)
+        {
+            float stats = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(stats);
+            return 0;
+        }
+    }
+}
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System;
 
 namespace Assignment1
 {
@@ -6,7 +9,29 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string integersTest = "25\n26\n10\n22\n16";
+            string statsTest = "12.5\n25.262\n12\n10008.2\n1224.2";
+
+            byte[] buffer1 = Encoding.UTF8.GetBytes(integersTest);
+            byte[] buffer2 = Encoding.UTF8.GetBytes(statsTest);
+
+            using (StreamReader READER1 = new StreamReader(new MemoryStream(buffer1)))
+            using (StreamReader READER2 = new StreamReader(new MemoryStream(buffer2)))
+
+            using (StreamWriter WRITER = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
+            {
+                Console.WriteLine("Enter your 5 digits for PrintIntegers():");
+                Assignment1.PrintIntegers(READER1, WRITER);
+
+                Console.WriteLine("Enter your 5 digits for PrintStats():");
+                Assignment1.PrintStats(READER2, WRITER);
+            }
+
+            Console.WriteLine("Test Assignment 1\n");
+
+
+
         }
     }
 }
