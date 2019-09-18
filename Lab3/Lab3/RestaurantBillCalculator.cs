13c5ffebedbc6c1174723f96404ff7d878cc1a63
RGasion <53403285+RGasion@users.noreply.github.com>
2019-09-19 07:06:12 +0900

    실습3 1차 업로드

---
+++ Lab3/Lab3/RestaurantBillCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Lab3
+{
+    public static class RestaurantBillCalculator
+    {
+        public static double CalculateTotalCost(StreamReader input)
+        {
+            double totalPrice = 0;
+            for (int i = 0 ; i < 5; i++)
+            {
+                totalPrice += double.Parse(input.ReadLine());
+            }
+
+            double tip = (100 + double.Parse(input.ReadLine())) / 100;
+
+            const double TAX = 1.05;
+            return Math.Round(totalPrice * tip * TAX, 2);
+        }
+
+        public static double CalculateIndividualCost(StreamReader input, double totalCost)
+        {
+            double people = double.Parse(input.ReadLine());
+
+            return Math.Round(totalCost / people, 2);
+        }
+
+        public static uint CalculatePayerCount(StreamReader input, double totalCost)
+        {
+            double pay = double.Parse(input.ReadLine());
+
+            return (uint)(totalCost % pay);
+        }
+    }
+}
