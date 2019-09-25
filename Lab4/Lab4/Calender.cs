59ba939e55695c3a15cb74a84b05c5c5e265c8e4
RGasion <53403285+RGasion@users.noreply.github.com>
2019-09-25 13:50:48 +0900

    실습4 1차 업로드

---
+++ Lab4/Lab4/Calender.cs
@@ -0,0 +1,52 @@
+﻿namespace Lab4
+{
+    public static class Calendar
+    {
+        public static bool IsLeapYear(uint year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else if (year % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int GetDaysInMonth(uint year, uint month)
+        {
+            if (year > 9999 || month < 1 || month > 12)
+            {
+                return -1;
+            }
+            else if (month == 2)
+            {
+                if (IsLeapYear(year) == true)
+                {
+                    return 29;
+                }
+                else
+                {
+                    return 28;
+                }
+            }
+            else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                return 31;
+            }
+            else
+            {
+                return 30;
+            }
+        }
+    }
+}
