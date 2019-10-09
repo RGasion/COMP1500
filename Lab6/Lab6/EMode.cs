ac8262f83881602fae9adb1cc25beb105b4d5cd4
RGasion <53403285+RGasion@users.noreply.github.com>
2019-10-10 07:40:13 +0900

    실습6 1차 업로드
    1. Rotate90Degrees 구현

---
+++ Lab6/Lab6/EMode.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6
+{
+    public enum EMode { HorizontalMirror, VerticalMirror, DiagonalShift };
+}
---
+++ Lab6/Lab6/Lab6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6
+{
+    class Lab6
+    {
+        public static int[,] Rotate90Degrees(int[,] arr)
+        {
+            if (arr.Length == 0)
+            {
+                return arr;
+            }
+            int originRow = arr.GetUpperBound(0) + 1;
+            int originColumn = arr.Length / originRow;
+
+            int[,] arrReturn = new int[originColumn, originRow];
+
+            for (int copyColumn = 0; copyColumn < originRow; copyColumn++)
+            {
+                for(int copyRow = 0; copyRow < originColumn; copyRow++)
+                {
+                    arrReturn[copyRow, arr.GetUpperBound(0) - copyColumn] = arr[copyColumn, copyRow];
+                }
+            }
+
+            return arrReturn;
+        }
+
+        public static void TransformArray(int[,] arr, EMode mode)
+        {
+            if (mode == EMode.HorizontalMirror)
+            {
+
+            }
+            else if (mode == EMode.VerticalMirror)
+            {
+
+            }
+            else if (mode == EMode.DiagonalShift)
+            {
+
+            }
+        }
+    }
+}
