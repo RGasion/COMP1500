93934cc61d89f8ffa5b84642fbda9d26bddd7cd5
RGasion <53403285+RGasion@users.noreply.github.com>
2019-11-17 09:12:59 +0900

    과제3 2차 업로드
    2. SolveTowerOfHanoi 구상 및 조건 미충족시 반환 설정

--- Assignment3/Assignment3/TowerOfHanoi.cs
+++ Assignment3/Assignment3/TowerOfHanoi.cs
@@ -2,6 +2,7 @@
 
 namespace Assignment3
 {
+    enum Direction { Center, Right}
     public static class TowerOfHanoi
     {
 
@@ -22,7 +23,28 @@ namespace Assignment3
 
         public static List<List<int>[]> SolveTowerOfHanoi(int numDiscs)
         {
-            return null;
+            List<List<int>[]> field = new List<List<int>[]>();
+
+            if (!(numDiscs > 0))
+            {
+                return field;
+            }
+            return field;
+            /*
+            //상태 초기화
+            List<int>[] towerChk = field[0];            
+            for (int i = numDiscs; i > 0; i--)
+            {
+                field[0][i - numDiscs] = i;
+            }
+            return null;*/
         }
+        /*
+        //특정 갯수의 원판들을 이동시키는 함수
+        public static List<List<int>[]> RecursiveMove(int numDiscs, Direction direction, List<List<int>[]> field)
+        {
+            Move(numDiscs - 1, Direction.Center, field);
+            return null;
+        }*/
     }
 }
