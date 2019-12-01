5e24cfdd2114a99a37b41d554f01d1152467784c
RGasion <53403285+RGasion@users.noreply.github.com>
2019-12-01 16:01:13 +0900

    실습11 1차 업로드
    1. 인코드 함수 구현

---
+++ Lab11/Lab11/Encoder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Lab11
+{
+    public static class Encoder
+    {
+        public static bool TryEncode(Stream input, Stream output)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+
+            const int MAX_COUNT = 255;
+
+            int count = 0;
+            char baseChar = '\0';
+            char checkChar;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (count == 0)
+                {
+                    baseChar = (char)input.ReadByte();
+
+                    count++;
+                }
+                else
+                {
+                    checkChar = (char)input.ReadByte();
+                    if (checkChar == baseChar)
+                    {
+                        count++;
+                    }
+                    else if (checkChar != baseChar || count == MAX_COUNT || i == input.Length - 1)
+                    {
+                        output.WriteByte((byte)count);
+                        output.WriteByte((byte)baseChar);
+
+                        count = 0;
+                    }
+                }
+            }
+
+            input.Close();
+            output.Close();
+
+            return true;
+        }
+
+        public static bool TryDecode(Stream input, Stream output)
+        {
+            return false;
+        }
+
+    }
+}
+
